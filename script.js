23d688f0ec1333cb6da3802b25b61619d8d74181
Arshynbeek <justarshynbek@gmail.com>
2024-02-27 23:50:44 +0600

    now?

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function sendMessage() {
   var name = document.getElementById("MessageName").value;
   var number = document.getElementById("MessageNumber").value;
-  var whatsappUrl = `https://wa.me/77075416503?text=Здравствуйте!,%0a*· Ф.И.О:* ${encodeURIComponent(name)}!; %0a*· Телефон:* ${encodeURIComponent(number)}`;
+  var whatsappUrl = `https://wa.me/77005009598?text=Здравствуйте!,%0a*· Ф.И.О:* ${encodeURIComponent(name)}!; %0a*· Телефон:* ${encodeURIComponent(number)}`;
   window.open(whatsappUrl, '_blank');
 }
